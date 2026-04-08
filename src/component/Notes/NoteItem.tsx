fdf8238c3ca3682e7caa9c921eaa20a28fa3c7f4
Priyanshi729 <priyanshisaini792@gmail.com>
2026-04-08 11:45:06 +0530

    made change

--- src/component/Notes/NoteItem.tsx
+++ src/component/Notes/NoteItem.tsx
@@ -15,7 +15,7 @@ const NoteItem: React.FC = () => {
   const [loading, setLoading] = useState(false);
 
   const [page, setPage] = useState(1);
-  const limit = 10;
+  const limit = 15;
   const [hasMore, setHasMore] = useState(true);
 
   const observerRef = useRef<HTMLDivElement | null>(null);
